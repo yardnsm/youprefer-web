80d4de9ca32ceb7a8c72a4d1797cd6c3047b140d
Yarden Sod-Moriah <yardnsm@gmail.com>
2022-10-23 07:46:48 +0300

    Upgrade to Firebase v9 (#44)

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,8 +1,11 @@
-import firebase from 'firebase/app';
-import 'firebase/database';
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
 
 // The firebase config is in that file. Good luck dudes.
 import config from './app-config';
 
-firebase.initializeApp(config.firebase);
+const firebase = initializeApp(config.firebase);
+const database = getDatabase(firebase);
+
+export { database };
 export default firebase;
